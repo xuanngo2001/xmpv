cd914bfbedcdc97d0afb8c2238f774b2da8b27a6
limelime <limelime@users.noreply.github.com>
2015-04-06 13:25:03 -0400

    Improve comment about usage.

--- xmpv.lua
+++ xmpv.lua
@@ -1,10 +1,11 @@
 -- DESCRIPTION: xmpv.lua integrates MPV and TMSU to provide the following features:
 --		-Tag files that you liked.
 -- USAGE:
---	Alt+l: Increment likes.
---	Alt+d: Decrement likes.
---	Alt+r: Reset likes to zero.
---	Alt+i: Print info.
+--		Hot keys:
+--			Alt+l: Increment likes.
+--			Alt+d: Decrement likes.
+--			Alt+r: Reset likes to zero.
+--			Alt+i: Print info.
 
 -- INSTALL: This script should be copied to ~/root/.config/mpv/scripts/ directory.
 -- REFERENCE: http://bamos.github.io/2014/07/05/mpv-lua-scripting/
