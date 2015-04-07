c85e59fa5d37df3f383927df3edbfe3a9b9f81bf
limelime <limelime@users.noreply.github.com>
2015-04-07 14:20:22 -0400

    emphasize keyboard.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DESCRIPTION
-  `xmpv.lua` is an extension of **MPV** that integrates with **TMSU** to provide the following features:
+`xmpv.lua` is an extension of **MPV** that integrates with **TMSU** to provide the following features:
   
   * Tag files that you liked.
   * Display your favorite files.
@@ -10,15 +10,15 @@
 
 # USAGE
 * Hot keys:
-  * Alt+l: Increment likes.
-  * Alt+d: Decrement likes.
-  * Alt+r: Reset likes to zero.
-  * Alt+i: Print info.
-  * Alt+t: Print top favorites files.
-  * Alt+m: Mark time position.
-  * Alt+n: Play next marked time position.
-  * Alt+b: Play previous marked time position.
-  * Alt+v: Delete previous marked time position.  
+  * `Alt+l`: Increment likes.
+  * `Alt+d`: Decrement likes.
+  * `Alt+r`: Reset likes to zero.
+  * `Alt+i`: Print info.
+  * `Alt+t`: Print top favorites files.
+  * `Alt+m`: Mark time position.
+  * `Alt+n`: Play next marked time position.
+  * `Alt+b`: Play previous marked time position.
+  * `Alt+v`: Delete previous marked time position.  
 
 # INSTALL
 This script should be copied to `~/.config/mpv/scripts/` directory.
--- xmpv.lua
+++ xmpv.lua
@@ -1,33 +1,34 @@
 --[[
 # DESCRIPTION
-  `xmpv.lua` is an extension of **MPV** that integrates with **TMSU** to provide the following features:
-    * Tag files that you liked.
-    * Display your favorite files.
-    * Mark time position.
-    * Play next marked time position.
-    * Play previous marked time position.
-    * Delete previous marked time position.
+`xmpv.lua` is an extension of **MPV** that integrates with **TMSU** to provide the following features:
+  
+  * Tag files that you liked.
+  * Display your favorite files.
+  * Mark time position.
+  * Play next marked time position.
+  * Play previous marked time position.
+  * Delete previous marked time position.
 
 # USAGE
-  * Hot keys:
-    * Alt+l: Increment likes.
-    * Alt+d: Decrement likes.
-    * Alt+r: Reset likes to zero.
-    * Alt+i: Print info.
-    * Alt+t: Print top favorites files.
-    * Alt+m: Mark time position.
-    * Alt+n: Play next marked time position.
-    * Alt+b: Play previous marked time position.
-    * Alt+v: Delete previous marked time position.  
+* Hot keys:
+  * `Alt+l`: Increment likes.
+  * `Alt+d`: Decrement likes.
+  * `Alt+r`: Reset likes to zero.
+  * `Alt+i`: Print info.
+  * `Alt+t`: Print top favorites files.
+  * `Alt+m`: Mark time position.
+  * `Alt+n`: Play next marked time position.
+  * `Alt+b`: Play previous marked time position.
+  * `Alt+v`: Delete previous marked time position.  
 
 # INSTALL
-  This script should be copied to `~/.config/mpv/scripts/` directory.
+This script should be copied to `~/.config/mpv/scripts/` directory.
 
 # REFERENCE: 
-  * http://bamos.github.io/2014/07/05/mpv-lua-scripting/
-  * https://github.com/mpv-player/mpv/blob/master/DOCS/man/lua.rst
-  * https://github.com/mpv-player/mpv/blob/master/DOCS/man/input.rst
-  * `mpv --list-properties` lists all properties available.
+* http://bamos.github.io/2014/07/05/mpv-lua-scripting/
+* https://github.com/mpv-player/mpv/blob/master/DOCS/man/lua.rst
+* https://github.com/mpv-player/mpv/blob/master/DOCS/man/input.rst
+* `mpv --list-properties` lists all properties available.
 
 ]]--
 
