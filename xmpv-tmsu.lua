a0669a605e593cf2090187a56dde613d16f6dc04
limelime <limelime@users.noreply.github.com>
2015-04-08 16:00:47 -0400

    initial OO transformation of TMSU.

---
+++ xmpv-tmsu.lua
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------------
+-- Tmsu class will manipulate TMSU application. 
+-----------------------------------------------------------------------------
+
+-- ***** Variables *****
+Tmsu = {
+  
+}
+
+-- ***** Functions *****
+function Tmsu:tag(tag_name, tag_value, cmd_file_path)
+  local cmd_tag = string.format("tmsu tag --tags=\"%s=%s\" %s", tag_name, tag_value, cmd_file_path)
+  execute_command(cmd_tag)
+end
+
+function Tmsu:untag(tag_name, tag_value, cmd_file_path)
+  local cmd_untag = string.format("tmsu untag --tags=\"%s=%s\" %s", tag_name, tag_value, cmd_file_path)
+  execute_command(cmd_untag)
+end
+
+-- Return raw tags, unformatted from TMSU.
+function Tmsu:get_tags()
+  -- Get tags of current file: tmsu tags <filename>
+  local cmd_get_tags = string.format("tmsu tags %s", file_name_for_cmd)
+  return execute_command(cmd_get_tags)  
+
+end
+
+-- Check if TMSU application exists.
+function Tmsu:exists()
+  local cmd_get_tmsu_version = "tmsu --version"
+  local cmd_results = execute_command(cmd_get_tmsu_version)
+  
+  if (string.find(cmd_results, "TMSU")==nil) then
+    local message =            string.format("ERROR: %s can't run.\n", mp.get_script_name())
+          message = message .. string.format("ERROR: It requires TMSU. Download it at http://tmsu.org/.")
+    mp.msg.error(message)
+  end 
+end
