f14f6185c5225ace6313cba7fb74a99fe262ff52
limelime <limelime@users.noreply.github.com>
2015-04-10 17:01:20 -0400

    Initial implementation of Msg class.

---
+++ xmpv-msg.lua
@@ -0,0 +1,30 @@
+-----------------------------------------------------------------------------
+-- Msg class. 
+-----------------------------------------------------------------------------
+
+-- ***** Variables *****
+Msg = {
+  duration = 1,
+}
+
+-- 'Constructor'
+function Msg:new(o)
+  o = o or {}
+  setmetatable(o, self)
+  self.__index = self
+  
+  return o
+end
+
+function Mark:print(con_text, osd_text)
+  
+  if(con_text~=nil) then
+    mp.msg.info(con_text)
+  end
+
+  if(osd_text~=nil) then
+    mp.osd_message(osd_text, self.duration)
+  end
+  
+end
+
