78a0a505fded171d999cdfe762c19db691b34e46
limelime <limelime@users.noreply.github.com>
2015-07-20 12:35:44 -0400

    Disable copy of unit tests.

--- xmpv-install.sh
+++ xmpv-install.sh
@@ -20,5 +20,5 @@ yes | cp *.lua ${LUA_DIR}
 ### Unit tests
 #rm -f ${LUA_DIR=}/xmpv-unit-tests.lua
 
-yes | cp ${TEST_DIR}/xmpv-unit-tests.lua ${LUA_DIR}
-yes | cp ${TEST_DIR}/luaunit.lua ${LUA_DIR}
+#yes | cp ${TEST_DIR}/xmpv-unit-tests.lua ${LUA_DIR}
+#yes | cp ${TEST_DIR}/luaunit.lua ${LUA_DIR}
