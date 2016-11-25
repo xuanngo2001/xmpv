78ae2ea1f402994bfa97697bf371e13fb64aa120
limelime <limelime@users.noreply.github.com>
2016-11-25 15:22:43 -0500

    Implement basic functions.

--- xmpv-asst.lua
+++ xmpv-asst.lua
@@ -15,15 +15,27 @@ function Asst:new(o)
 end
 
 -- -------------------------------- --
--- FUNCTIONS --
+-- BASIC FUNCTIONS --
 -- -------------------------------- --
 
--- Enable ASS tag by returning on code. 
-function Asst:on()
-  return mp.get_property("osd-ass-cc/0")
-end
+function Asst:on()  return mp.get_property("osd-ass-cc/0") end
+function Asst:off() return mp.get_property("osd-ass-cc/1") end
 
--- Disable ASS tag by returning off code. 
-function Asst:off()
-  return mp.get_property("osd-ass-cc/1")
+function Asst:reset() return "{\\r}" end
+
+function Asst:underline_o() return "{\\u1}" end
+function Asst:underline_f() return "{\\u0}" end
+
+function Asst:font_size(fs) return "{\\fs" .. fs .. "}" end
+
+-- -------------------------------- --
+-- BIGGER FUNCTIONS --
+-- -------------------------------- --
+function Asst:long_text(text)
+  return string.format("%s%s%s%s%s", self:on(), 
+                                       self:font_size(12), 
+                                        text, 
+                                       self:reset(), 
+                                     self:off()
+                      )
 end
